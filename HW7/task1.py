e39ded88d0e7930ba8cb80ee7c0a085035cfaf18
Lobachev Evgeniy <e.lobachev@bk.ru>
2023-08-09 12:49:52 +0400

    task1

---
+++ HW7/task1.py
@@ -0,0 +1,28 @@
+import os
+
+dict_folder = {'mpeg': 'Video',
+               'mp4': 'Video',
+               'flv': 'Video',
+               'doc': 'Text Documents',
+               'txt': 'Text Documents',
+               'pdf': 'Text Documents',
+               'png': 'Images',
+               'rav': 'Images',
+               'psd': 'Images',
+               'jpg': 'Images',
+               }
+
+
+def sort_files_for_dir(path=""):
+    if path != "":
+        os.chdir(path)
+
+    list_files = os.listdir()
+    for item in list_files:
+        if os.path.isfile(item):
+            *_, exp_file = item.split('.')
+            name_folder = dict_folder.get(exp_file)
+            if name_folder is not None:
+                if not os.path.exists(name_folder):
+                    os.mkdir(name_folder)
+                os.replace(item, f'{name_folder}//{item}')
