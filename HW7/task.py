89b22101a5537ae800e946b9675d6f9ce53054a8
Lobachev Evgeniy <e.lobachev@bk.ru>
2023-08-09 13:05:43 +0400

    task

--- HW7/task.py
+++ HW7/task.py
@@ -22,3 +22,23 @@ def sort_files_for_dir(path=""):
 
 '''part2'''
 
+def group_rename_files(path='', postfix='', count_digit=4, exp_old='txt', exp_new='', prefix=(1, 3)):
+    list_files = os.listdir()
+    curr_count = 0
+    for item in list_files:
+        if os.path.isfile(item):
+            name_file, exp_file = item.split('.')
+            if exp_file == exp_old:
+                curr_count += 1
+                spam = str(format(curr_count, f'0>{count_digit}'))
+                eggs = exp_file if exp_new == "" else exp_new
+                new_name = f'{name_file[prefix[0]:prefix[1] + 1]}{postfix}{spam}.{eggs}'
+
+                os.rename(item, new_name)
+
+
+how_task = input("Какую функцию запустить?\n1 - Сортировка\n2 - Групповое переименование\nВведите 1 или 2: ")
+if how_task == '1':
+    sort_files_for_dir(path="")
+elif how_task == '2':
+    group_rename_files(path=str(os.chdir('Text Documents')), postfix='_RE_', exp_old='txt', exp_new='doc')
